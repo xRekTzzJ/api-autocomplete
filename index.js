787b864910142a0f12f7bc8140454d2eca16b816
Pavel Popov <pavel2003sbk@yandex.ru>
2024-02-24 04:50:39 +0800

    refactor: добавить подсказки и исправить мелкие баги

--- index.js
+++ index.js
@@ -2,73 +2,80 @@ const searchContainer = document.querySelector(".form__request-container");
 const cardContainer = document.querySelector(".card-container");
 const inputError = document.querySelector(".form__label");
 const input = document.querySelector(".form__input");
-
+//Задержка
 const debounce = (fn, ms) => {
   let timeout;
   return function () {
     const fnCall = () => {
-      fn.apply(this, arguments);
+      fn.apply(this);
     };
     clearTimeout(timeout);
     timeout = setTimeout(fnCall, ms);
   };
 };
+//Убрать поисковые запросы
 const clearSearches = () => {
-  document.querySelectorAll(".form__request").forEach((i) => i.remove());
+  searchContainer.querySelectorAll(".form__request").forEach((i) => i.remove());
 };
+//Удалить карточку
 const deleteCard = (e) => {
   if (e.target.closest(".card__delete-icon")) {
     e.target.closest(".card").remove();
   }
 };
+//Спрятать ошибку поиска
+const hideError = () => {
+  inputError.classList.remove("form__label_visible");
+};
+//Показать ошибку поиска
+const showError = () => {
+  inputError.classList.add("form__label_visible");
+};
+//Создать новую карточку
 const newCard = (i) => {
   clearSearches();
   input.value = "";
-  const newCard = document
+  const cardElement = cardContainer
     .querySelector("#card-template")
     .content.querySelector(".card")
     .cloneNode(true);
-  const [name, owner, stars] = newCard.querySelectorAll(".card__info");
+  const [name, owner, stars] = cardElement.querySelectorAll(".card__info");
   name.textContent = `Name: ${i.name}`;
   owner.textContent = `Owner: ${i.owner.login}`;
   stars.textContent = `Stars: ${i.stargazers_count}`;
-  cardContainer.append(newCard);
+  cardContainer.append(cardElement);
 };
+//Логика поиска
 async function search() {
   if (input.value === "") {
     clearSearches();
-    inputError.classList.remove("form__label_visible");
+    hideError();
     return;
   }
   const result = await fetch(
     `https://api.github.com/search/repositories?q=${input.value}`
   ).then((res) => res.json());
-  inputError.classList.remove("form__label_visible");
+  hideError();
   clearSearches();
   const [one, two, three, four, five, ...others] = result.items;
   let arr = [one, two, three, four, five];
   if (one === undefined) {
-    inputError.classList.add("form__label-visible");
+    showError();
     return;
   }
   arr.forEach((i) => {
-    const searchItem = document
+    const searchItem = searchContainer
       .querySelector("#request-template")
       .content.querySelector(".form__request")
       .cloneNode(true);
     searchItem.textContent = i.name;
     searchContainer.append(searchItem);
-    searchItem.addEventListener(
-      "click",
-      () => {
-        newCard(i);
-      },
-      {
-        once: true,
-      }
-    );
+    searchItem.addEventListener("click", () => newCard(i), {
+      once: true,
+    });
   });
 }
+//Слушатели
 input.addEventListener("keyup", debounce(search, 700), {
   passive: true,
 });
